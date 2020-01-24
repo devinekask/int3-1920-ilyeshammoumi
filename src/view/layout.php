c3f96eec9464630241477bdf3071e2649c9c2d59
ilyeshammoumi <ilyes.hammoumi@gmail.com>
2020-01-24 03:01:13 +0100

    roboto url nog toegevoegd

--- src/view/layout.php
+++ src/view/layout.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://fonts.googleapis.com/css?family=Cormorant+Infant|Delius+Unicase|Oswald|Pacifico|Rosarivo|Bebas+Neue&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Cormorant+Infant|Delius+Unicase|Oswald|Pacifico|Rosarivo|Bebas+Neue|Roboto&display=swap" rel="stylesheet">
     <title>Humo - webshop</title>
      <?php echo $css;?>
   </head>
