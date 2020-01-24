908ce5b5fb6d0a59eb58451c22244cdd1a12ab66
ilyeshammoumi <ilyes.hammoumi@gmail.com>
2020-01-24 01:53:52 +0100

    longread mobile en tablet zijn gecodeerd

--- src/view/products/longread.php
+++ src/view/products/longread.php
@@ -5,16 +5,131 @@
   <p  class="lr--itro__link oswald__font">Ontmoet Katy</p>
 </a>
 </header>
-<section class="scene_1">
+<section class="lr--scene_1">
   <div class="scene_1--wrapper">
+  <div class="scene_1--texts__wrapper">
   <h2 class="hidden">Mijn naam is Katy</h2>
   <p class="oswald__font scene_1--text scene1-alinea1">Ik ben <span class="pacifico__font scene-1__katy">Katy</span> de schattige, blauwharige baby in de armen van mama en papa.  Mijn <span class="scene-luke"> STOERE PAPA LUKE</span>, mijn <span class="delius__font scene-june"> Lieve Mama June </span>, eenvoudige maar trotse ouders van hun eerstgeborene. Zoals vele jongen mensen zijn zij heel moedig hun leven aan het opbouwen en lopen zij over van liefde en vreugde voor elkaar. </p>
 
   <p class="oswald__font scene_1--text"><span class="pacifico__font scene1-alinea2">Broertjes en Zusjes ?</span> <br> Ik ben er van overtuigd, er zullen er nog heel wat volgen want een groot gezin is hun grootste ambitie. Wij wonen in een mooi, proper huisje. Als mama en papa naar hun werk gaan zorgt mijn oma’tje voor mij. Ik amuseer me rot maar ben o zo blij ben als ik ’s avonds terug naar huis mag. </p>
-
+  </div>
   <div class="scene_1-ouders">
     <img src="./assets/longread/scene_1/pijlen.png" alt="pijlen" height="260" class="scene_1-pijlen">
+
   </div>
   </div>
-  
+
+</section>
+
+<section class="lr--scene_2">
+  <img src="./assets/longread/scene_2/wolken.png" alt="wolken" class="lr--scene2__clouts"  height="231">
+  <h2 class="hidden">Leren fietsen</h2>
+  <div class="lr--scene_2--images">
+  <div class="lr--scene_2--texts scene_2--desktop">
+      <p class="scene_2--mobile__alinea1 oswald__font ">
+      Binnen ons gezin is het niet altijd roze geur en mannenschijn. Het grote gezin is uiteindelijk uit gebleven. Niet omdat mama of papa niet genoeg liefde in hun hart hebben om nog andere kindjes graag te zien maar gewoon omdat dit hen werd opgelegd. Wat is mama hier verdrietig om. Uren, nachten na elkaar heb ik haar vanuit mijn bedje horen wenen, <span class="scene_2--alinea1__keyword">SCHREEUWEND</span> tegen papa hoe <span class="scene_2--alinea1__keyword"> ONRECHTVAARDIG </span> het leven wel is.
+      </p>
+      <p class="scene_2--mobile__alinea1 oswald__font scene_2__alinea--rechts">
+      Toch doen mama en papa er alles aan om mij gelukkig te maken. Met de meest eenvoudige dingen <span class="pacifico__font scene_2--alinea2__keyword ">maken zij me heel gelukkig</span>. Papa mijn superheld die me leert fietsen. Mijn allerliefste en zachte mama die ons een heerlijke picknick klaarmaakt om samen van te genieten na een wandeling in het nabije bos.
+      </p>
+  </div>
+    <img src="./assets/longread/scene_2/june.png" alt="June" class="scene_2--june" width="75.5" height="133">
+    <img src="./assets/longread/scene_2/katy-bike.png" alt="Katy" class="scene_2--katy__bike" width="64" height="95">
+    <div class="swipe--flex">
+    <img src="./assets/longread/scene_2/swipe.png" alt="Swipe finger" class="scene_2--swipe" width="71" height="84">
+    </div>
+    <img src="./assets/longread/scene_2/luke.png" alt="Luke" class="scene_2--luke" width="79" height="133">
+  </div>
+  <div class="lr--scene_2--texts scene_2--mobile">
+      <p class="scene_2--mobile__alinea1 oswald__font">
+      Binnen ons gezin is het niet altijd roze geur en mannenschijn. Het grote gezin is uiteindelijk uit gebleven. Niet omdat mama of papa niet genoeg liefde in hun hart hebben om nog andere kindjes graag te zien maar gewoon omdat dit hen werd opgelegd. Wat is mama hier verdrietig om. Uren, nachten na elkaar heb ik haar vanuit mijn bedje horen wenen, <span class="scene_2--alinea1__keyword">SCHREEUWEND</span> tegen papa hoe <span class="scene_2--alinea1__keyword"> ONRECHTVAARDIG </span> het leven wel is.
+      </p>
+      <p class="scene_2--mobile__alinea1 oswald__font">
+      Toch doen mama en papa er alles aan om mij gelukkig te maken. Met de meest eenvoudige dingen <span class="pacifico__font scene_2--alinea2__keyword ">maken zij me heel gelukkig</span>. Papa mijn superheld die me leert fietsen. Mijn allerliefste en zachte mama die ons een heerlijke picknick klaarmaakt om samen van te genieten na een wandeling in het nabije bos.
+      </p>
+  </div>
+
+</section>
+<img src="./assets/longread/scene_2/gras.png" alt="gras" class="lr--scene2__gras">
+<section class="lr--scene_3">
+  <div class="lr--scene_3--texts">
+  <p class="scene_3--text oswald__font">
+      Mama wordt als maar verdrietiger en zieker, papa als maar radelozer. En zij niet alleen want ook mijn vriendjes en vriendinnetjes horen hun mama en papa <span class="rosarivo__font scene_3--alinea1__keyword">RUZIE MAKEN EN SAMEN HUILEN</span> . Ik begrijp het allemaal niet zo goed. Papa leest mij een briefje voor dat ze die dag in de brievenbus hebben gekregen.
+      </p>
+      <p class="scene_3--text oswald__font">
+      Wetenschappers hebben ontdekt dat al de mensen op aarde onze planeet heel erg <span class="rosarivo__font scene_3--alinea2__keyword">VERVUILEN</span> en dat we daardoor veel vlugger ziek worden en vooral veel minder sterk zijn. Mama’s hebben <span class="rosarivo__font scene_3--alinea2__keyword"> DODE KINDJES </span>in hun buik of kunnen er gewoon geen meer hebben en zo begrijp ik ook waarom ik geen broertjes of zusjes heb gekregen.
+      </p>
+      </div>
+<div class="scene_3--images">
+  <div class="scene_3__part--left">
+      <img src="./assets/longread/scene_3/baby--left.png" alt="baby" class="baby--left" width="96" height="183">
+      <img src="./assets/longread/scene_3/man--left.png" alt="man" class="man--left" width="143" height="207">
+  </div>
+  <div class="scene_3__part--centrum">
+    <img src="./assets/longread/scene_3/women--after.png" alt="zwangere vrouw" class="zwanger--centrum" height="348.5" width="166.6rem" >
+  </div>
+  <div class="scene3_3__part--right">
+    <img src="./assets/longread/scene_3/women--right.png" alt="wenende vrouw" class="women--right" width="74.6" height="171">
+    <img src="./assets/longread/scene_3/dokter--right.png" alt="dokter" class="dokter--right" width="61" height="187">
+
+  </div>
+</div>
+</section>
+<section class="lr--scene_4">
+  <div class="lr--scene_4--texts">
+    <p class="scene_4--text oswald__font">
+   <span class="rosarivo__font scene_4--keyword1">Mijn oma’tje is overleden want ze was ook heel erg ziek geworden...</span> <br>
+Ik ben boos en verdrietig. Christelijke mensen willen ons land redden en gaan nu beslissen wat wij wel en niet meer mogen doen. Misschien zal mama nu weer blij worden en nog een baby’tje krijgen. Ik hoor dat veel mensen toch niet blij zijn met die beslissingen en gaan op straat lopen om te roepen, om te protesteren. Opeens hoort ik vanuit ons huis, <span class="scene_4--keyword2">LUIDE KNALLEN</span>, <span class="scene_4--keyword2">SCHREEUWEN</span>, <span class="scene_4--keyword2">ROEPEN</span>, <span class="scene_4--keyword2">HUILEN</span>. De papa van mijn vriendje, Rob, is door de soldaten van de Christelijke mensen <span class="scene_4--keyword2">DOOD GESCHOTEN</span>.
+    </p>
+  </div>
+  <div class="lr--scene_4--images">
+    <img src="./assets/longread/scene_4/priester.png" alt="prietser" class="scene_4--priester" width="41" height="140">
+  </div>
+</section>
+<section class="lr--scene_5">
+  <div class="lr--scene_5--wrapper">
+    <div class="lr--scene_5__charachter scene_5__luke">
+      <img src="./assets/longread/scene_5/luke_dark.jpg"  class="scene_5__charachter--bottom" alt="luke dark" width=100% class>
+      <img src="./assets/longread/scene_5/luke_light.jpg" class="scene_5__charachter--top" alt="luke light" width=100% >
+      <div class="scene_5--button__bgi button__bgi--yellow">
+        <button type="button" class="scene_5--button rosarivo__font">Wat ben ik geworden ?</button>
+      </div>
+    </div>
+    <div class="lr--scene_5__charachter scene_5__katy">
+      <img src="./assets/longread/scene_5/katy_dark.jpg"  class="scene_5__charachter--bottom" alt="katy dark" width=100% class>
+      <img src="./assets/longread/scene_5/katy_light.jpg" class="scene_5__charachter--top" alt="katy light" width=100% >
+      <div class="scene_5--button__bgi button__bgi--green">
+        <button type="button" class="scene_5--button rosarivo__font">Wat ben ik geworden ?</button>
+      </div>
+      </div>
+      <div class="lr--scene_5__charachter scene_5__june">
+      <img src="./assets/longread/scene_5/june_dark.png"  class="scene_5__charachter--bottom" alt="june dark" width=100% >
+     <img src="./assets/longread/scene_5/june_light.jpg" class="scene_5__charachter--top june" alt="june light" width=100% >
+      <div class="scene_5--button__bgi button__bgi--bleu">
+        <button type="button" class="scene_5--button rosarivo__font">Wat ben ik geworden ?</button>
+      </div>
+    </div>
+    </div>
+    <div class="lr--scene_5--texts">
+    <p class="scene_4--text scene_5--text oswald__font">
+    Ons gezinnetje bestaat niet meer. Papa werkt en woont nu in en nabij de <span class="scene_5--keyword__red"> KERNCENTRALE </span>. Wanneer hij toch eens <span class="scene_5--keyword__darkred"> KWAAD</span> en <span class="scene_5--keyword__yellow"> OPSTANDIG</span> wordt dan wordt hij in een werkkamp geplaatst. Het leven is er nog ongezonder dan in ons knusse huisje van vroeger.
+    </p>
+    <p class="scene_4--text scene_5--text oswald__font">
+    Mama krijgt inderdaad nog andere baby’tjes maar het zijn nooit mijn broer of zusjes. Mijn mama wordt een “<span class="scene_5--keyword__red">HANDMAIDS TALE</span>”, een dienstmeid. Het wordt haar taak om gezonde <span class="scene_5--keyword__yellow"> KINDJES OP DE WERELD ZETTEN</span>. Zij ziet ze helaas nooit want van zodra de kindjes geboren worden, verdwijnen ze weer en worden ze naar andere gezinnen gebracht.
+    </p>
+    </div>
+</section>
+<section class="lr--scene_6">
+  <div class="lr--scene_6-images">
+      <img src="./assets/longread/scene_6/june.png" alt="June" class="scene_6--june" width="197" height="402">
+      <img src="./assets/longread/scene_6/front.png" alt="front" class="scene_6--front" width="644" height=470>
+  </div>
+</section>
+<section class="lr--scene_5--texts scene_6--texts">
+  <p class="scene_4--text scene_5--text oswald__font">
+  Mijn mama is zo’n <span class="scene_5--keyword__red">STERKE </span> vrouw. Ze heeft zoveel ellende meegemaakt. Jaren leeft zij nu al op dezelfde manier, jaren heeft zij mij niet gezien en moeten missen. Toch heeft zij nooit willen opgeven. Zoals velen leeft zij in een heel streng regime maar niets is voor altijd. Ze vindt <span class="scene_5--keyword__yellow">EEN FALING IN HET SYSTEEM </span>en ontdekt een rebellen groep.
+    </p>
+    <p class="scene_4--text scene_6--text oswald__font">
+    Ze sluit zich aan bij de groep en neemt actief deel aan zoveel mogelijk activiteiten . Alleen zo kan zij misschien aan haar lot ontsnappen en <span class="scene_5--keyword__red"> OP ZOEK </span> gaan naar haar <span class="scene_5--keyword__pink rosarivo__font">DOCHTER EN ECHTGENOOT</span>. Haar allergrootste probleem is dat de wereld groot is en zij absoluut niet weet waar ze haar zoektocht moet starten.
+    </p>
 </section>
